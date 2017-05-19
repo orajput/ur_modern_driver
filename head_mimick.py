ed74b6dd363e5ee1f3700e35f5587badc4ab24d2
Omer Rajput <omer.rajput@tuhh.de>
2017-05-19 18:14:30 +0200

    Head mimick trajectory
    data and launch files

--- head_mimick.py
+++ head_mimick.py
@@ -10,23 +10,6 @@ from math import pi
 import numpy as np
 import scipy.io as sio
 import socket
-headTrajMatFile = '/home/omer/Repositories/Kinect/matlab/TMS/headTrajSystematic.mat' #sys.argv[1]
-speedSc = 1.5
-ang6_offset = pi/6.
-#numSampsToUse = 200
-headTrajData = sio.loadmat(headTrajMatFile)
-startJoints = headTrajData['jInit']*pi/180.
-startJoints[5,0] = startJoints[5,0]-ang6_offset
-#jointSpeeds = headTrajData['jjSp']*pi/180.
-#time_intervals = headTrajData['tIntSelSc']
-time_intervals = headTrajData['tIntFull']
-#numTrajPts = headTrajData['numTrajPts']
-jjAll = headTrajData['jjFull']*pi/180.
-jjAll[5,:] = jjAll[5,:] - ang6_offset
-#jjAll = jjAll[:,:numSampsToUse]
-#time_intervals = time_intervals[:numSampsToUse-1]*speedSc
-time_intervals[0,50:1250] = time_intervals[0,50:1250]*speedSc
-time_start = np.append([0],time_intervals.cumsum())
 
 JOINT_NAMES = ['shoulder_pan_joint', 'shoulder_lift_joint', 'elbow_joint',
                'wrist_1_joint', 'wrist_2_joint', 'wrist_3_joint']
@@ -35,7 +18,28 @@ Q2 = [1.5,0,-1.57,0,0,0]
 Q3 = [1.5,-0.2,-1.57,0,0,0]
 
 client = None
+jjAll = None
+time_start = None
+startJoints = None
 
+def read_head_data(headTrajMatFile, speedSc, ang6_offset):
+    global jjAll
+    global time_start
+    global startJoints
+    # numSampsToUse = 200
+    headTrajData = sio.loadmat(headTrajMatFile)
+    startJoints = headTrajData['jInit'] * pi / 180.
+    startJoints[5, 0] = startJoints[5, 0] - ang6_offset
+    # jointSpeeds = headTrajData['jjSp']*pi/180.
+    # time_intervals = headTrajData['tIntSelSc']
+    time_intervals = headTrajData['tIntFull']
+    # numTrajPts = headTrajData['numTrajPts']
+    jjAll = headTrajData['jjFull'] * pi / 180.
+    jjAll[5, :] = jjAll[5, :] - ang6_offset
+    # jjAll = jjAll[:,:numSampsToUse]
+    # time_intervals = time_intervals[:numSampsToUse-1]*speedSc
+    time_intervals[0, 50:1250] = time_intervals[0, 50:1250] * speedSc
+    time_start = np.append([0], time_intervals.cumsum())
 
 def move_head_traj():
     global joints_pos
@@ -178,9 +182,7 @@ def move_interrupt():
 
 def main():
     global client
-    ip_ctrlMaster = '134.28.45.36'
-    port_ctrlMaster = 2525
-    numCases = 1
+    global data_path
     try:
         rospy.init_node("head_mimick", anonymous=True, disable_signals=True)
         client = actionlib.SimpleActionClient('/follow_joint_trajectory', FollowJointTrajectoryAction)
@@ -189,16 +191,25 @@ def main():
         print "Connected to server"
         parameters = rospy.get_param(None)
         index = str(parameters).find('prefix')
+        headTrajMatFile = rospy.get_param('~head_traj_mat_file','/home/omer/Repositories/Kinect/matlab/TMS/headTrajSystematic.mat')
+        useCtrlMaster = rospy.get_param('~use_ctrl_master',False)
+        ip_ctrlMaster = rospy.get_param('~ip_ctrl_master')
+        port_ctrlMaster = rospy.get_param('~port_ctrl_master',2525)
+        numCases = rospy.get_param('~num_cases',1)
+        speedSc = rospy.get_param('~speed_scale',1.5)
+        ang6_offset = rospy.get_param('~ang6_offset_deg', 30)*pi/180
+        read_head_data(headTrajMatFile, speedSc, ang6_offset)
         if (index > 0):
             prefix = str(parameters)[index+len("prefix': '"):(index+len("prefix': '")+str(parameters)[index+len("prefix': '"):-1].find("'"))]
             for i, name in enumerate(JOINT_NAMES):
                 JOINT_NAMES[i] = prefix + name
         #
-        ctrlMaster = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        #
-        # adjust IP in the driver.txt to the IP of the computer(when  using the
-        # simulator make sure that you take the ip of the computer in the virtual % network!)
-        ctrlMaster.connect((ip_ctrlMaster, port_ctrlMaster))
+        if useCtrlMaster:
+            ctrlMaster = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            #
+            # adjust IP in the driver.txt to the IP of the computer(when  using the
+            # simulator make sure that you take the ip of the computer in the virtual % network!)
+            ctrlMaster.connect((ip_ctrlMaster, port_ctrlMaster))
         # make sure it is stopped and no other program is running
         print "This program makes the robot move between the following three poses:"
         print str([Q1[i]*180./pi for i in xrange(0,6)])
@@ -209,7 +220,8 @@ def main():
         if (inp == 'y'):
             for c in range(numCases):
                 t0 = time.time()
-                #ctrlMaster.send('start\n')
+                if useCtrlMaster:
+                    ctrlMaster.send('start\n')
                 move_start_pos()
                 move_head_traj()
                 t1 = time.time()
@@ -221,7 +233,8 @@ def main():
             #move_interrupt()
         else:
             print "Halting program"
-        ctrlMaster.close()
+        if useCtrlMaster:
+            ctrlMaster.close()
     except KeyboardInterrupt:
         rospy.signal_shutdown("KeyboardInterrupt")
         raise
