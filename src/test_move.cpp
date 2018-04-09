dd10117f8850e5c81c753b00ec5ee548b5c98e73
Omer Rajput <omer.rajput@tuhh.de>
2018-04-09 19:24:50 +0200

    test move cpp parameters added

--- src/test_move.cpp
+++ src/test_move.cpp
@@ -13,10 +13,11 @@ private:
   ros::NodeHandle nh_, priv_nh_;
   std::vector<std::string> joint_names_;
   unsigned int num_joints_;
+  std::string action_server_name_;
 public:
   //! Initialize the action client and wait for action server to come up
-  RobotArm(const ros::NodeHandle &nh = ros::NodeHandle(), const ros::NodeHandle &priv_nh = ros::NodeHandle("~"))
-    : nh_(nh), priv_nh_(priv_nh), num_joints_(6)
+  RobotArm(const std::string & action_server_name, const ros::NodeHandle &nh = ros::NodeHandle(), const ros::NodeHandle &priv_nh = ros::NodeHandle("~"))
+    : nh_(nh), priv_nh_(priv_nh), num_joints_(6), action_server_name_ (action_server_name)
   {
     joint_names_.resize(num_joints_);
     joint_names_[0] = "shoulder_pan_joint";
@@ -27,7 +28,7 @@ public:
     joint_names_[5] = "wrist_3_joint";
 
     // tell the action client that we want to spin a thread by default
-    traj_client_ = new TrajClient(nh_, std::string("vel_based_pos_traj_controller/follow_joint_trajectory"), true);
+    traj_client_ = new TrajClient(nh_, action_server_name_, true);
 
     // wait for action server to come up
     while(!traj_client_->waitForServer(ros::Duration(5.0))){
@@ -56,9 +57,9 @@ public:
       be in its own trajectory - a trajectory can have one or more waypoints
       depending on the desired application.
   */
-  control_msgs::FollowJointTrajectoryGoal simpleTrajectory(double dir_offset)
+  control_msgs::FollowJointTrajectoryGoal simpleTrajectory(double joint_move_dist, double traj_time)
   {
-    //sensor_msgs::JointStateConstPtr msg = ros::topic::waitForMessage<sensor_msgs::JointState>(std::string("joint_states"), nh_);
+    sensor_msgs::JointStateConstPtr msg = ros::topic::waitForMessage<sensor_msgs::JointState>(std::string("joint_states"), nh_);
 
     //our goal variable
     control_msgs::FollowJointTrajectoryGoal goal;
@@ -67,25 +68,24 @@ public:
     goal.trajectory.joint_names = joint_names_;
 
     // We will have two waypoints in this goal trajectory
-    goal.trajectory.points.resize(1);
+    goal.trajectory.points.resize(2);
 
-    double start_times[2] = {0.0, 1.0};
-    double joint_offset[2] = {0.0, dir_offset*0.02};
-    double start_joints[6] = {1.079542636871338, -1.2710145155536097, -1.7220404783831995, -1.1891167799579065, 1.6462156772613525, -2.2031338850604456};
+    double start_times[2] = {0.0, traj_time};
+    double joint_offset[2] = {0.0, joint_move_dist};
     // Trajectory points
-    for (size_t ind = 0; ind < 1; ++ind)
+    for (size_t ind = 0; ind < 2; ++ind)
     {
       goal.trajectory.points[ind].positions.resize(6);
       goal.trajectory.points[ind].velocities.resize(6);
       for (size_t j = 0; j < num_joints_; ++j)
       {
         // Positions
-        goal.trajectory.points[ind].positions[j] = start_joints[j] + joint_offset[ind+1];
+        goal.trajectory.points[ind].positions[j] = msg->position[j] + joint_offset[ind];
         // Velocities
         goal.trajectory.points[ind].velocities[j] = 0.0;
       }
       // To be reached 1 second after starting along the trajectory
-      goal.trajectory.points[ind].time_from_start = ros::Duration(start_times[ind+1]);
+      goal.trajectory.points[ind].time_from_start = ros::Duration(start_times[ind]);
     }
 
     //we are done; return the goal
@@ -105,22 +105,38 @@ int main(int argc, char** argv)
   // Init the ROS node
   ros::init(argc, argv, "simple_trajectory_player");
 
-  RobotArm arm;
-  double dir_offset = 1.0;
-  ros::Rate r(1); // 1 hz
+  // Name of the action server offered by the ur_modern_driver
+  // "follow_joint_trajectory" -> servoj based control
+  // "vel_based_pos_traj_controller/follow_joint_trajectory" -> speedj based control
+  std::string action_server_name = "follow_joint_trajectory";
+  ros::param::get("~action_server_name", action_server_name);
+  ROS_INFO_STREAM("action_server_name: " << action_server_name);
+
+  // Amount of movement in each joint
+  double joint_move_dist = 30.0;
+  ros::param::get("~joint_move_dist", joint_move_dist);
+  ROS_INFO_STREAM("joint_move_dist: " << joint_move_dist);
+  joint_move_dist = joint_move_dist*M_PI/180.0; // deg to rad
+
+  // Amount of time to complete the movement
+  double traj_time = 1.0;
+  ros::param::get("~traj_time", traj_time);
+  ROS_INFO_STREAM("traj_time: " << traj_time);
+
+  RobotArm arm(action_server_name);
   while (ros::ok())
   {
     // Start the trajectory
-    arm.startTrajectory(arm.simpleTrajectory(dir_offset));
+    arm.startTrajectory(arm.simpleTrajectory(joint_move_dist, traj_time));
     // Wait for trajectory completion
-//    while(!arm.getState().isDone() && ros::ok())
-//    {
-//      ros::Duration(0.05).sleep();
-//      ros::spinOnce();
-//    }
-    r.sleep();
+    while(!arm.getState().isDone() && ros::ok())
+    {
+      ros::Duration(0.05).sleep();
+      ros::spinOnce();
+    }
     ros::spinOnce();
-    dir_offset = dir_offset * -1;
+    // to move back and forth
+    joint_move_dist = joint_move_dist * -1;
   }
   return 0;
 }
